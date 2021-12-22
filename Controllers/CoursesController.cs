07f206cdb2b3c7244538767a7f803edae999c14f
Prospect <M-Essam1111>
2021-12-22 16:59:12 +0200

    fix

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -309,10 +309,10 @@ namespace BrAcademy.Controllers
         }
 
 
-        [HttpPost]
-        public IActionResult CropCourseImage_NotWorking(string ImageName, int width, int height, int x, int y)
+        //[HttpPost]
+        //public IActionResult CropCourseImage_NotWorking(string ImageName, int width, int height, int x, int y)
 
-        {
+        //{
             // string ImageUrl = Request.QueryString("url");
             // string NewFileName;
             //if (ImageUrl.EndsWith("jpeg"))
@@ -365,69 +365,69 @@ namespace BrAcademy.Controllers
             //        return Ok();
             //    }
             //}
-            return Ok();
+        //    return Ok();
 
-        }
+        //}
 
         
 
 
 
-        [HttpPost]
-        public IActionResult CropCourseImage(string filename, IFormFile blob)
-        {
-            try
-            {
-                using (var image = Image.Load(blob.OpenReadStream()))
-                {
-                    string uniqueFileName = Guid.NewGuid().ToString();
-                   // var path = Path.Combine(_env.WebRootPath, "images", "Courses", uniqueFileName.Substring(0, 5) + "_" + file.FileName);
-                    string systemFileExtenstion = filename.Substring(filename.LastIndexOf('.'));
+        //[HttpPost]
+        //public IActionResult CropCourseImage(string filename, IFormFile blob)
+        //{
+        //    try
+        //    {
+        //        using (var image = Image.Load(blob.OpenReadStream()))
+        //        {
+        //            string uniqueFileName = Guid.NewGuid().ToString();
+        //           // var path = Path.Combine(_env.WebRootPath, "images", "Courses", uniqueFileName.Substring(0, 5) + "_" + file.FileName);
+        //            string systemFileExtenstion = filename.Substring(filename.LastIndexOf('.'));
 
                    
-                    var newfileName180 = filename+"_" +uniqueFileName.Substring(0, 5);
-                    var filepath160 = Path.Combine(_env.WebRootPath, "images", "Courses", newfileName180);
-                    image.Mutate(x => x.Resize(600, 600));
-                    image.Save(filepath160);
+        //            var newfileName180 = filename+"_" +uniqueFileName.Substring(0, 5);
+        //            var filepath160 = Path.Combine(_env.WebRootPath, "images", "Courses", newfileName180);
+        //            image.Mutate(x => x.Resize(600, 600));
+        //            image.Save(filepath160);
 
-                    //var newfileName200 = GenerateFileName("Photo_200_200_", systemFileExtenstion);
-                    //var filepath200 = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Images")).Root + $@"\{newfileName200}";
-                    //image.Mutate(x => x.Resize(200, 200));
-                    //image.Save(filepath200);
+        //            //var newfileName200 = GenerateFileName("Photo_200_200_", systemFileExtenstion);
+        //            //var filepath200 = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Images")).Root + $@"\{newfileName200}";
+        //            //image.Mutate(x => x.Resize(200, 200));
+        //            //image.Save(filepath200);
 
-                    //var newfileName32 = GenerateFileName("Photo_32_32_", systemFileExtenstion);
-                    //var filepath32 = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Images")).Root + $@"\{newfileName32}";
-                    //image.Mutate(x => x.Resize(32, 32));
-                    //image.Save(filepath32);
+        //            //var newfileName32 = GenerateFileName("Photo_32_32_", systemFileExtenstion);
+        //            //var filepath32 = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Images")).Root + $@"\{newfileName32}";
+        //            //image.Mutate(x => x.Resize(32, 32));
+        //            //image.Save(filepath32);
 
-                }
+        //        }
 
-                return Json(new { Message = "OK" });
-            }
-            catch (Exception)
-            {
-                return Json(new { Message = "ERROR" });
-            }
-        }
+        //        return Json(new { Message = "OK" });
+        //    }
+        //    catch (Exception)
+        //    {
+        //        return Json(new { Message = "ERROR" });
+        //    }
+        //}
         //public string GenerateFileName(string fileTypeName, string fileextenstion)
         //{
         //    if (fileTypeName == null) throw new ArgumentNullException(nameof(fileTypeName));
         //    if (fileextenstion == null) throw new ArgumentNullException(nameof(fileextenstion));
         //    return $"{fileTypeName}_{DateTime.Now:yyyyMMddHHmmssfff}_{Guid.NewGuid():N}{fileextenstion}";
         //}
-        [HttpPost]
-        public IActionResult SaveCroppedImage()
-        {
-            string base64 = Request.Form["imgCropped"];
-            byte[] bytes = Convert.FromBase64String(base64.Split(',')[1]);
-            var newfileName = Guid.NewGuid().ToString()+".jpg";
-            string filePath = Path.Combine(_env.WebRootPath, "images", "Courses", newfileName);
-            using (FileStream stream = new FileStream(filePath, FileMode.Create))
-            {
-                stream.Write(bytes, 0, bytes.Length);
-                stream.Flush();
-            }
-            return Ok();
-        }
+        //[HttpPost]
+        //public IActionResult SaveCroppedImage()
+        //{
+        //    string base64 = Request.Form["imgCropped"];
+        //    byte[] bytes = Convert.FromBase64String(base64.Split(',')[1]);
+        //    var newfileName = Guid.NewGuid().ToString()+".jpg";
+        //    string filePath = Path.Combine(_env.WebRootPath, "images", "Courses", newfileName);
+        //    using (FileStream stream = new FileStream(filePath, FileMode.Create))
+        //    {
+        //        stream.Write(bytes, 0, bytes.Length);
+        //        stream.Flush();
+        //    }
+        //    return Ok();
+        //}
     }
 }

c2cb9d83ddac509758a27a18e1b0eff242c80aaf
mohamed essm <langara5000@hotmail.com>
2024-07-03 00:30:37 +0300

    video

--- wwwroot/js/CreateCourse.js
+++ wwwroot/js/CreateCourse.js
@@ -11,7 +11,7 @@
         .catch(error => {
             console.error(error);
         });
-        ////
+        //////
     //ClassicEditor
     //    .create(document.querySelector('#editor'), {
     //        plugins: [
